e9add70a9e08d7beb72438faef0e483211ca1203
excitedCPU <gxt13@mails.tsinghua.edu.cn>
2015-11-24 00:05:42 +0800

    manager to be done

--- adapter/PC.vhd
+++ adapter/PC.vhd
@@ -3,6 +3,8 @@ use IEEE.SED_LOGIC_1164.ALL;
 
 entity PC is
 	port (
+		clk: in std_logic;
+		rst: in std_logic;
 		from_mux_pc: in std_logic;
 		from_riskCheck: in std_logic;
 		pc_out: out std_logic_vector(15 downto 0)
@@ -10,7 +12,6 @@ entity PC is
 end PC;
 
 architecture behavioral of PC is
-
 begin
 
 end architecture ; -- behavioral
