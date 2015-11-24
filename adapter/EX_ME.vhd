5b931fb555b16bab86fe07de3bbf8f856825cfb8
excitedCPU <gxt13@mails.tsinghua.edu.cn>
2015-11-24 23:23:31 +0800

    adapter finished

--- adapter/EX_ME.vhd
+++ adapter/EX_ME.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity EX_ME is
 	port (
+		clk, rst: in std_logic;
 		control_memToReg_in: in std_logic;
 		control_regWrite_in: in std_logic_vector(2 downto 0);
 		control_memRead_in: in std_logic_vector(2 downto 0);
@@ -25,5 +26,27 @@ end EX_ME;
 
 architecture behavioral of EX_ME is
 begin
+	process(clk, rst)
+	begin
+		if rst = '1' then
+			control_memToReg_out <= '0';
+			control_regWrite_out <= "000";
+			control_memRead_out <= "000";
+			control_memWrite_out <= "000";
+			from_branch_control_out <= '0';
+			desReg_out <= "0000000000000000";
+			ram_addr_out <= "0000000000000000";
+			ALUresult_out <= "0000000000000000";
+		elsif clk'event and clk = '1' then
+			control_memToReg_out <= control_memToReg_in;
+			control_regWrite_out <= control_regWrite_in;
+			control_memRead_out <= control_memRead_in;
+			control_memWrite_out <= control_memWrite_in;
+			from_branch_control_out <= from_branch_control_in;
+			desReg_out <= desReg_in;
+			ram_addr_out <= ram_addr_in;
+			ALUresult_out <= ALUresult_in;
+		end if;
+	end process;
 
 end architecture ; -- behavioral
