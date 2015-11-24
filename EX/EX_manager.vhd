3164bdb7f960af7a20eaca38dfd99664c007d632
Eric <wavegu@126.com>
2015-11-24 11:31:32 +0800

    change EX_manager port

--- EX/EX_manager.vhd
+++ EX/EX_manager.vhd
@@ -1,5 +1,5 @@
 library IEEE;
-use IEEE.SED_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity EX_manager is
 	port (
@@ -15,22 +15,33 @@ entity EX_manager is
 		control_WE: in std_logic;
 		control_TE: in std_logic;
 		updated_pc: in std_logic_vector(15 downto 0);
-		from_register1: in std_logic_vector(15 downto 0);
-		from_register2: in std_logic_vector(15 downto 0);
 		from_HI_SP: in std_logic_vector(15 downto 0);
 		from_imm_exp: in std_logic_vector(15 downto 0);
 		from_T: in std_logic_vector(15 downto 0);
 		from_instruction_7_5: in std_logic_vector(2 downto 0);
 		from_instruction_4_2: in std_logic_vector(2 downto 0);
 		from_instruction_10_8: in std_logic_vector(2 downto 0);
+		from_byPass_in: in std_logic;
+		ME_result: in std_logic_vector(15 downto 0);
+		ALUresult_from_EX_ME: in std_logic_vector(15 downto 0);
+		register_read_result1: in std_logic_vector(15 downto 0);
+		register_read_result2: in std_logic_vector(15 downto 0);
+
 
 		out_memToReg: out std_logic_vector(2 downto 0);
 		out_regWrite: out std_logic_vector(2 downto 0);
 		out_memWrite: out std_logic;
 		out_memRead: out std_logic;
-		ALUresult: out std_logic_vector(15 downto 0);
-		mux_writeRamData: out std_logic_vector(15 downto 0);
-		mux_desRegister: out std_logic_vector(15 downto 0)
+		out_ALUresult: out std_logic_vector(15 downto 0);
+		out_mux_writeRamData: out std_logic_vector(15 downto 0);
+		out_mux_desRegister: out std_logic_vector(15 downto 0);
+		out_from_instruction_10_8: out std_logic_vector(2 downto 0);
+		out_from_instruction_7_5: out std_logic_vector(2 downto 0);
+		out_byPass_to_branchControl: out std_logic;
+		out_ID_EX_to_branchControl: out std_logic;
+		out_control_branchOrJump: in std_logic_vector(2 downto 0);
+		out_register_read_result1: in std_logic_vector(15 downto 0);
+
 		);
 end EX_manager;
 
