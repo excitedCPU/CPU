d4083fd67feda2f45a1e2b503002c6a5bac5c815
excitedCPU <gxt13@mails.tsinghua.edu.cn>
2015-11-24 22:55:18 +0800

    EX mannager to do

--- EX/EX_manager.vhd
+++ EX/EX_manager.vhd
@@ -15,15 +15,17 @@ entity EX_manager is
 		control_WE: in std_logic;
 		control_TE: in std_logic;
 		updated_pc: in std_logic_vector(15 downto 0);
-		from_HI_SP: in std_logic_vector(15 downto 0);
+		from_HI: in std_logic_vector(15 downto 0);
+		from_SP: in std_logic_vector(15 downto 0);
 		from_imm_exp: in std_logic_vector(15 downto 0);
 		from_T: in std_logic_vector(15 downto 0);
 		from_instruction_7_5: in std_logic_vector(2 downto 0);
 		from_instruction_4_2: in std_logic_vector(2 downto 0);
 		from_instruction_10_8: in std_logic_vector(2 downto 0);
-		from_byPass_in: in std_logic;
-		ME_result: in std_logic_vector(15 downto 0);
-		ALUresult_from_EX_ME: in std_logic_vector(15 downto 0);
+		Fsrc1: in std_logic_vector(1 downto 0);
+		Fsrc2: in std_logic_vector(1 downto 0);
+		result_from_ME_WB: in std_logic_vector(15 downto 0);
+		result_from_EX_ME: in std_logic_vector(15 downto 0);
 		register_read_result1: in std_logic_vector(15 downto 0);
 		register_read_result2: in std_logic_vector(15 downto 0);
 
@@ -39,8 +41,9 @@ entity EX_manager is
 		out_from_instruction_7_5: out std_logic_vector(2 downto 0);
 		out_byPass_to_branchControl: out std_logic;
 		out_ID_EX_to_branchControl: out std_logic;
-		out_control_branchOrJump: in std_logic_vector(2 downto 0);
-		out_register_read_result1: in std_logic_vector(15 downto 0)
+		out_control_branchOrJump: out std_logic_vector(2 downto 0);
+		out_register_read_result1: out std_logic_vector(15 downto 0);
+		out_sum: out std_logic_vector(15 downto 0);
 		);
 end EX_manager;
 
@@ -85,43 +88,47 @@ architecture structural of EX_manager is
 		result : OUT std_logic_vector(15 downto 0)
 		);
 	END COMPONENT;
+	
+	--inner signal
+	signal alu_src1: std_logic_vector(15 downto 0);
+	signal alu_src2: std_logic_vector(15 downto 0);
 begin
 	Inst_ALU: ALU PORT MAP(
-		ALUsrc1 => ,
-		ALUsrc2 => ,
-		control_ALUop => ,
-		result => ,
-		sum => 
+		ALUsrc1 => alu_src1,
+		ALUsrc2 => alu_src2,
+		control_ALUop => control_ALUop,
+		result => out_ALUresult,
+		sum => out_sum
 	);
 	Inst_mux_ALUsrc1: mux_ALUsrc PORT MAP(
-		control_ALUsrc => ,
-		control_Fsrc => ,
-		from_pc => ,
-		from_HI => ,
-		from_SP => ,
-		from_EX_ME => ,
-		from_ME_WB => ,
-		from_imm_exp => ,
-		from_register => ,
-		result => 
+		control_ALUsrc => control_ALUsrc1,
+		control_Fsrc => Fsrc1,
+		from_pc => updated_pc,
+		from_HI => from_HI,
+		from_SP => from_SP,
+		from_EX_ME => result_from_EX_ME,
+		from_ME_WB => result_from_ME_WB,
+		from_imm_exp => from_imm_exp,
+		from_register => register_read_result1,
+		result => alu_src1
 	);
 	Inst_mux_ALUsrc2: mux_ALUsrc PORT MAP(
-		control_ALUsrc => ,
-		control_Fsrc => ,
-		from_pc => ,
-		from_HI => ,
-		from_SP => ,
-		from_EX_ME => ,
-		from_ME_WB => ,
-		from_imm_exp => ,
-		from_register => ,
-		result => 
+		control_ALUsrc => control_ALUsrc2,
+		control_Fsrc => Fsrc2,
+		from_pc => updated_pc,
+		from_HI => from_HI,
+		from_SP => from_SP,
+		from_EX_ME => result_from_EX_ME,
+		from_ME_WB => result_from_ME_WB,
+		from_imm_exp => from_imm_exp,
+		from_register => register_read_result2,
+		result => alu_src2
 	);
 	Inst_mux_desRegister: mux_desRegister PORT MAP(
-		instruction_7_5 => ,
-		instruction_4_2 => ,
-		instruction_10_8 => ,
-		control_regDest => ,
+		instruction_7_5 => from_instruction_7_5,
+		instruction_4_2 => from_instruction_4_2,
+		instruction_10_8 => from_instruction_10_8,
+		control_regDest => control_desRegister,
 		desRegister => 
 	);
 	Inst_mux_writeRamData: mux_writeRamData PORT MAP(
