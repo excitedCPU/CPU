6533763c61e4457e679b516190d3128660cf0043
Eric <wavegu@126.com>
2015-11-25 00:10:27 +0800

    ram read data test ok

--- ME/ME_manager.vhd
+++ ME/ME_manager.vhd
@@ -5,16 +5,20 @@ entity ME_manager is
 	port (
 		clk: in std_logic;
 		rst: in std_logic;
-		control_memToReg: in std_logic;
-		control_memRead: in std_logic;
-		control_memWrite: in std_logic;
-		control_regWrite_in: in std_logic_vector(2 downto 0);
-		ALUresult_in: in std_logic_vector(15 downto 0);
+		--control_memToReg: in std_logic;
+		--control_memRead: in std_logic;
+		--control_memWrite: in std_logic;
+		--control_regWrite_in: in std_logic_vector(2 downto 0);
+		--ALUresult_in: in std_logic_vector(15 downto 0);
 		ram_addr: in std_logic_vector(15 downto 0);
-		desReg_in: in std_logic_vector(15 downto 0);
-		desReg_out: out std_logic_vector(15 downto 0);
+		--desReg_in: in std_logic_vector(2 downto 0);
+		--desReg_out: out std_logic_vector(2 downto 0);
 		result_out: out std_logic_vector(15 downto 0);
-		control_regWrite_out: out std_logic_vector(2 downto 0)
+		--control_regWrite_out: out std_logic_vector(2 downto 0)
+		C_14: out std_logic;
+		OE_L: out std_logic;
+		WE_L: out std_logic;
+		ram_en: out std_logic
 		);
 end ME_manager;
 
@@ -25,11 +29,11 @@ architecture behavioral of ME_manager is
 			clk: in std_logic;
 			rst: in std_logic;
 			data: inout std_logic_vector(15 downto 0);
-			addr: in std_logic_vector(15 downto 0);
 			read_enable: in std_logic;
 			write_enable: in std_logic;
 			OE_L: out std_logic;
 			WE_L: out std_logic;
+			ram_addr_out: out std_logic_vector(17 downto 0);
 			ram_out: out std_logic_vector(15 downto 0)
 		);		
 	end component;
@@ -43,20 +47,28 @@ architecture behavioral of ME_manager is
 		);
 	end component;
 
-
-	signal OE_L, WE_L: std_logic;
 	signal inout_signal: std_logic_vector(15 downto 0);
 	signal ramResult: std_logic_vector(15 downto 0);
+
+	signal control_memRead: std_logic := '1';
+	signal control_memWrite: std_logic := '0';
+	signal control_memToReg: std_logic := '1';
+	signal ALUresult_in: std_logic_vector(15 downto 0) := "1111111111111111";
+	signal tem_ram_addr: std_logic_vector(17 downto 0);
 begin
+	ram_en <= '0';
+	tem_ram_addr(15 downto 0) <= ram_addr;
+	tem_ram_addr(17 downto 16) <= "00";
+
 	Inst_ram: ram PORT MAP(
 		clk => clk,
 		rst => rst,
 		data => inout_signal,
-		addr => ram_addr,
 		read_enable => control_memRead,
 		write_enable => control_memWrite,
 		OE_L => OE_L,
 		WE_L => WE_L,
+		ram_addr_out => tem_ram_addr,
 		ram_out => ramResult
 	);
 	Inst_mux_wbData: mux_wbData PORT MAP(
@@ -66,14 +78,15 @@ begin
 		result => result_out
 	);
 
-	desReg_out <= desReg_in;
-	control_regWrite_out <= control_regWrite_in;
+	--desReg_out <= desReg_in;
+	--control_regWrite_out <= control_regWrite_in;
 	
 	process(control_memWrite, inout_signal, ALUresult_in)
 	begin
 		if control_memWrite = '1' then
 			inout_signal <= ALUresult_in;
 		end if;
+		C_14 <= '1';
 	end process;
 	
 end architecture ; -- behavioral
--- ME/ram.vhd
+++ ME/ram.vhd
@@ -5,12 +5,14 @@ entity ram is
 	port (
 		clk: in std_logic;
 		rst: in std_logic;
+		ram_addr_in: in std_logic_vector(15 downto 0);
+		--read_enable: in std_logic;
+		--write_enable: in std_logic;
+		ram1_en: out std_logic;
 		data: inout std_logic_vector(15 downto 0);
-		addr: in std_logic_vector(15 downto 0);
-		read_enable: in std_logic;
-		write_enable: in std_logic;
 		OE_L: out std_logic;
 		WE_L: out std_logic;
+		ram_addr_out: out std_logic_vector(17 downto 0);
 		ram_out: out std_logic_vector(15 downto 0)
 		);
 end ram;
@@ -21,8 +23,15 @@ architecture behavioral of ram is
 	type status is (WORK, RESTORE);
 	signal current_status: status := WORK;
 	signal next_status: status := WORK;
+	signal read_enable: std_logic := '1';
+	signal write_enable: std_logic := '0';
 
 begin
+	read_enable <= '1';
+	write_enable <= '0';
+	ram1_en <= '0';
+	ram_addr_out(17 downto 16) <= "00";
+	ram_addr_out(15 downto 0) <= ram_addr_in;
 
 	time_control: process(clk, rst)
 	begin
@@ -45,7 +54,7 @@ begin
 		end case;
 	end process;
 
-	logic_control: process(clk, current_status, data, addr, read_enable, write_enable)
+	logic_control: process(clk, current_status, data, read_enable, write_enable)
 	begin
 		if clk'event and clk = '0' then
 			case current_status is
@@ -63,5 +72,7 @@ begin
 			end case;
 		end if;
 	end process;
+
+	ram_out <= data;
 
 end architecture ; -- behavioral
