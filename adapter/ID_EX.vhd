105b0edd06020505f36525fe5ddb02e3742c9839
Mfhraven <mfhraven@126.com>
2015-11-25 17:52:44 +0800

    lump the EX and ID

--- adapter/ID_EX.vhd
+++ adapter/ID_EX.vhd
@@ -10,7 +10,7 @@ entity ID_EX is
 		control_ALUsrc1_in: in std_logic_vector(2 downto 0);
 		control_ALUsrc2_in: in std_logic_vector(2 downto 0);
 		control_ALUop_in: in std_logic_vector(3 downto 0);
-		control_memToReg_in: in std_logic_vector(2 downto 0);
+		control_memToReg_in: in std_logic;
 		control_regWrite_in: in std_logic_vector(2 downto 0);
 		control_memWrite_in: in std_logic;
 		control_memRead_in: in std_logic;
@@ -19,7 +19,8 @@ entity ID_EX is
 		updated_pc_in: in std_logic_vector(15 downto 0);
 		from_register1_in: in std_logic_vector(15 downto 0);
 		from_register2_in: in std_logic_vector(15 downto 0);
-		from_HI_SP_in: in std_logic_vector(15 downto 0);
+		from_IH_in: in std_logic_vector(15 downto 0);
+		from_SP_in: in std_logic_vector(15 downto 0);
 		from_imm_exp_in: in std_logic_vector(15 downto 0);
 		from_T_in: in std_logic_vector(15 downto 0);
 		from_instruction_7_5_in: in std_logic_vector(2 downto 0);
@@ -31,7 +32,7 @@ entity ID_EX is
 		control_ALUsrc1_out: out std_logic_vector(2 downto 0);
 		control_ALUsrc2_out: out std_logic_vector(2 downto 0);
 		control_ALUop_out: out std_logic_vector(3 downto 0);
-		control_memToReg_out: out std_logic_vector(2 downto 0);
+		control_memToReg_out: out std_logic;
 		control_regWrite_out: out std_logic_vector(2 downto 0);
 		control_memWrite_out: out std_logic;
 		control_memRead_out: out std_logic;
@@ -40,7 +41,8 @@ entity ID_EX is
 		updated_pc_out: out std_logic_vector(15 downto 0);
 		from_register1_out: out std_logic_vector(15 downto 0);
 		from_register2_out: out std_logic_vector(15 downto 0);
-		from_HI_SP_out: out std_logic_vector(15 downto 0);
+		from_IH_out: out std_logic_vector(15 downto 0);
+		from_SP_out: out std_logic_vector(15 downto 0);
 		from_imm_exp_out: out std_logic_vector(15 downto 0);
 		from_T_out: out std_logic_vector(15 downto 0);
 		from_instruction_7_5_out: out std_logic_vector(2 downto 0);
@@ -58,11 +60,11 @@ begin
 			--all out signal set to zero
 			control_branchOrJump_out <= "000";
 			control_desRegister_out <= "00";
-			control_ALUsrc1_out <= "000";
-			control_ALUsrc2_out <= "000";
+			control_ALUsrc1_out <= "111";
+			control_ALUsrc2_out <= "111";
 			control_ALUop_out <= "0000";
-			control_memToReg_out <= "000";
-			control_regWrite_out <= "000";
+			control_memToReg_out <= '0';
+			control_regWrite_out <= "100";
 			control_memWrite_out <= '0';
 			control_memRead_out <= '0';
 			control_WE_out <= '0';
@@ -70,7 +72,8 @@ begin
 			updated_pc_out <= "0000000000000000";
 			from_register1_out <= "0000000000000000";
 			from_register2_out <= "0000000000000000";
-			from_HI_SP_out <= "0000000000000000";
+			from_IH_out <= "0000000000000000";
+			from_SP_out <= "0000000000000000";
 			from_imm_exp_out <= "0000000000000000";
 			from_T_out <= "0000000000000000";
 			from_instruction_7_5_out <= "000";
@@ -92,7 +95,8 @@ begin
 			updated_pc_out <= updated_pc_in;
 			from_register1_out <= from_register1_in;
 			from_register2_out <= from_register2_in;
-			from_HI_SP_out <= from_HI_SP_in;
+			from_IH_out<= from_IH_in;
+			from_SP_out<= from_SP_in;
 			from_imm_exp_out <= from_imm_exp_in;
 			from_T_out <= from_T_in;
 			from_instruction_7_5_out <= from_instruction_7_5_in;
