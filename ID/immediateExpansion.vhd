41f2060d56475e49a4e7b57961a501d401bde2a7
excitedCPU <gxt13@mails.tsinghua.edu.cn>
2015-11-25 17:26:47 +0800

    alu and immediateexpansion tested

--- ID/immediateExpansion.vhd
+++ ID/immediateExpansion.vhd
@@ -17,18 +17,32 @@ begin
 	begin
 		operation:= instruction(15 downto 11);
 		case operation is
+			when "01001" => --ADDIU
+				imm_exp_result <= std_logic_vector(resize(signed(instruction(7 downto 0)), 16));
 			when "01000" => --ADDIU3
 				imm_exp_result <= std_logic_vector(resize(signed(instruction(3 downto 0)), 16));
+			when "01100" => --ADDSP BTEQZ BTNEZ
+				imm_exp_result <= std_logic_vector(resize(signed(instruction(7 downto 0)), 16));
 			when "00010" => --B
 				imm_exp_result <= std_logic_vector(resize(signed(instruction(10 downto 0)), 16));
+			when "00100" => --BEQZ
+				imm_exp_result <= std_logic_vector(resize(signed(instruction(7 downto 0)), 16));
+			when "00101" => --BNEZ
+				imm_exp_result <= std_logic_vector(resize(signed(instruction(7 downto 0)), 16));
 			when "01101" => --LI
 				imm_exp_result <= std_logic_vector(resize(unsigned(instruction(7 downto 0)), 16));
+			when "10011" => --LW
+				imm_exp_result <= std_logic_vector(resize(signed(instruction(4 downto 0)), 16));
+			when "10010" => --LW_SP
+				imm_exp_result <= std_logic_vector(resize(signed(instruction(7 downto 0)), 16));
 			when "00110" => --SLL\SRA\SRL
 				imm_exp_result <= std_logic_vector(resize(unsigned(instruction(4 downto 2)), 16));
 			when "01011" => --SLTUI
 				imm_exp_result <= std_logic_vector(resize(unsigned(instruction(7 downto 0)), 16));
 			when "11011" => --SW
 				imm_exp_result <= std_logic_vector(resize(signed(instruction(4 downto 0)), 16));
+			when "11010" => --SW_SP
+				imm_exp_result <= std_logic_vector(resize(signed(instruction(7 downto 0)), 16));
 			when others => --other instruction and states
 				imm_exp_result <= std_logic_vector(resize(signed(instruction(7 downto 0)), 16));
 		end case;
