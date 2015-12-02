f1a59c648ae0fbe707ecafdc8542e6a5f86b12f1
Eric <wavegu@126.com>
2015-12-02 17:48:21 +0800

    add generate data script

---
+++ buffer/generate_init_buffer_ram_data.py
@@ -0,0 +1,16 @@
+def char_to_bin(ch):
+	bin_str = bin(ord(ch))[2:]
+	return '0'*(7-len(bin_str)) + bin_str
+
+def str_to_bin(string):
+	ans = ''
+	for ch in string:
+		ans += char_to_bin(ch)
+	return ans
+
+print str_to_bin('6766')
+
+with open('buffer_data.coe', 'w') as coe_file:
+	coe_file.write('memory_initialization_radix=2;\nmemory_initialization_vector=\n')
+	for looper in range(32):
+		coe_file.write(str_to_bin('0123456789ABCDEFGHIJKLMNOPQRSTUBWXWZ') + '0000,\n')
